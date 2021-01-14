03408a5bf0ca508782409dd900986a2df4f3c4d6
Gokul Kumar Gopal <gokulkumar.gopal>
2021-01-14 23:48:12 +0530

    Part A changes done

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+function closemodal(element) {
+    var modal_to_close = document.getElementById(element);
+    modal_to_close.style.display = "none";
+}
+
+function openmodal(element) {
+    var modal_to_close = document.getElementById(element);
+    modal_to_close.style.display = "block";
+}
+
+function invalidInput(input) {
+    if(input.value == '') {
+        input.setCustomValidity('Please fill out this field');
+    }
+}
